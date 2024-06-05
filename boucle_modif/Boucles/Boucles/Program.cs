a219fc3b0015f68803fc3c1a21424a8c0df7b893
remi1040 <48013030+remi1040@users.noreply.github.com>
2024-06-05 14:30:27 +0200

    Add files via upload

---
+++ boucle_modif/Boucles/Boucles/Program.cs
@@ -0,0 +1,33 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+String[] moves = new String[32];
+
+for (int i = 0; i < moves.Length; i++)
+{
+    Console.WriteLine("Entrez un pas de danse: "); 
+    string userEntry = Console.ReadLine();
+    if (userEntry == "done")
+    {
+        break;
+    }
+    else
+    {
+        moves[i] = userEntry;
+    }
+}
+
+Console.WriteLine("Entrez un nombre de répétition: ");
+string nombre_de_pas = Console.ReadLine();
+
+int number1 = Convert.ToInt32(nombre_de_pas);
+
+
+
+for (int i = 0; i < number1; i++) {
+    foreach (string move in moves)
+    {
+        Console.Write(move + " ");
+    }
+    Console.WriteLine();
+    
+}
